5853136814461db5f8adcf050530506b11065366
thefallingduck <bjlinzmeier@gmail.com>
2017-01-19 14:14:16 -0600

    Update spiralcenterfinder.r
    Changed to using radius and spearman rank order correlation to determine center of spiral. There should be a continuous monotonic increase with number away from the center.

--- spiralcenterfinder.r
+++ spiralcenterfinder.r
@@ -1,52 +1,56 @@
 spiralcenter <- function( data, step = 2){
-# Determines the center of a spiral made of limited points
-
-# Arguments:
-#  data:            dataframe or matrix of (x, y) points
-#  step:            step for search smaller runs longer
+  # Determines the center of a spiral made of limited points
   
-##### Internal 
-#  CentroidX:       centroid X for center of data cloud
-#  CentroidY:       centroid Y for center of data cloud
- 
-#  ClosestX:        closest X point to centroid from data
-#  ClosestY:        closest Y point to centroid from data
-
-# Returns:
-#  CenterPoint:    pair of points that identify the center
-source(angle.r)
-
-CentroidX <- mean(data$X)
-CentroidY <- mean(data$Y)
+  # Arguments:
+  #  data:            dataframe or matrix of (x, y) points
+  #  step:            step for search smaller runs longer
   
-ClosestX <- which.min(abs(data$X - CentroidX))
-ClosestY <- which.min(abs(data$Y - CentroidY))
-
-DistX <- abs(CentroidX - ClosestX)
-DistY <- abs(CentroidY - ClosestY)
-
-SearchX <- seq(from = (CentroidX - DistX), to = (CentroidX - DistX), seq = step)
-SearchY <- seq(from = (CentroidY - DistY), to = (CentroidY - DistY), seq = step)
-
-SearchGrid <- expand.grid(SearchX, SearchY)
-
-sumAngles <- vector(length = nrow(SearchGrid))
-
-for(k in 1:length(totalRotation)){
+  ##### Internal 
+  #  CentroidX:       centroid X for center of data cloud
+  #  CentroidY:       centroid Y for center of data cloud
   
-  for(j in 1:nrow(data)){
+  #  ClosestX:        closest X point to centroid from data
+  #  ClosestY:        closest Y point to centroid from data
+  
+  # Returns:
+  #  CenterPoint:    pair of points that identify the center
+  #source(angle.r)
+  
+  CentroidX <- mean(data$X)
+  CentroidY <- mean(data$Y)
+  
+  CentralX <- data$X[1]
+  CentralY <- data$Y[1]
+  
+  DistX <- abs(CentroidX - CentralX)
+  DistY <- abs(CentroidY - CentralY)
+  
+  SearchX <- seq(from = (CentralX - DistX), to = (CentralX + DistX), by = step)
+  SearchY <- seq(from = (CentralY - DistY), to = (CentralY + DistY), by = step)
+  
+  SearchGrid <- expand.grid(SearchX, SearchY)
+  
+  SpearmanDistance <- vector(length = nrow(SearchGrid))
+  
+  totalDistance <- matrix(nrow = length(data$X), ncol = length(SearchGrid))
+  
+  for(k in 1:length(SpearmanDistance)){
     
-    allangles <- rep(0, length = nrow(data))
-    allangles[j+1] <- angle(center = SearchGrid[k], pointOne = data[j, ], pointTwo = data[j+1, ])
-        
-      }
+    allDistance <- rep(0, length = nrow(data))
+    
+    for(j in 1:(length(data$X)-1)){
+      
+      allDistance[j+1] <- distance(pointOne = SearchGrid[k, ], pointTwo = data[j, ])
+      
+    }
+    
+    SpearmanDistance[k] <- cor(x = 1:length(allDistance), y = allDistance, method = 'spearman')
+    #totalDistance[,k] <- allDistance
   
-  sumAngles[k] <- sum(allangles)
-
   }
   
-CenterPoint <- SearchGrid[which.min(sumAngles),]
-
-return(CenterPoint)
-
+  CenterPoint <- SearchGrid[which.max(SpearmanDistance),]
+   
+  return(CenterPoint)
+  
 }
