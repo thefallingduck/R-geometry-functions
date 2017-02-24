af9148b04f3ffce70dc898708d0fe240dac18815
thefallingduck <bjlinzmeier@gmail.com>
2017-02-24 14:19:31 -0600

    Update errorbar.r
    fix issue with '...' use and also duplicate to create two error bar functions. One X one Y.

--- errorbar.r
+++ errorbar.r
@@ -1,4 +1,4 @@
-errorbar <- function(x, y, upper, lower = upper, length = 0.1, orientation = 90...){
+errorbarY <- function(x, y, upper, lower = upper, length = 0.1, orientation = 90, ...){
 
 #Plots error bars as arrows on a plot.
 #Arguments:
@@ -14,5 +14,24 @@ if(length(x) != length(y) | length(y) != length(lower) | length(lower) != length
 stop("vectors must be same length")
 
 arrows(x, y + upper, x, y - lower, angle = orientation, code = 3, length = length, ...)
+
+}
+
+errorbarX <- function(x, y, upper, lower = upper, length = 0.1, orientation = 90, ...){
+
+#Plots error bars as arrows on a plot.
+#Arguments:
+# x:              x coordinant of a point. Can be a vector paired with other points.
+# y:              y coordinant of a point. Can be a vector paired with other points.
+# upper:          Upper length of error bar.
+# lower:          Lower length of error bar.
+# length:         Length of the end of the error bars.
+# orientation:    Orientation of the error bars relative to the points.
+
+if(length(x) != length(y) | length(y) != length(lower) | length(lower) != length(upper))
+
+stop("vectors must be same length")
+
+arrows(x + upper, y, x - lower, y, angle = orientation, code = 3, length = length, ...)
 
 }
